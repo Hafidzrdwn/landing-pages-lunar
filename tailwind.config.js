e81f4fd0e97eb8d35dc01f739009ee6ae1b261c1
Hafidzrdwn <hafidz10032005@gmail.com>
2025-03-09 21:22:30 +0700

    feat: make a landing page consistent [color pallete and content]

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
    content: ["./src/**/*.{html,js}"],
    theme: {
-     extend: {},
-   },
+    extend: {
+      colors: {
+        blue: {
+          50: "#e6f0ff",  // Sangat terang
+          100: "#bfd8ff",
+          200: "#99c0ff",
+          300: "#73a8ff",
+          400: "#4d90ff",
+          500: "#004aad", // Base color
+          600: "#003e91",
+          700: "#003275",
+          800: "#002659",
+          900: "#001b3d",  // Sangat gelap
+        },
+      },
+    },
+  },
    plugins: [],
- }
+}
